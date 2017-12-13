36cfa7c7266864e4921ef8e0a141a4cd92795771
randy-darsh <rspringer47@gmail.com>
2017-12-13 09:58:12 -0700

    making station model

---
+++ app/models/station.rb
@@ -0,0 +1,10 @@
+class Station
+  def initialize(attributes = {})
+    @name = attributes[:name]
+    @address = attributes[:address]
+    @fuel_type_code = attributes[:fuel_type_code]
+    @access_days_time = attributes[:access_days_time]
+  end
+end
+
+    # @Name, Address, Fuel Types, Distance, and Access Times
