9e68a1f61cade04f23cb0eb89e39bb71d45a2c9a
randy-darsh <rspringer47@gmail.com>
2017-12-13 10:11:26 -0700

    working on nrel service

--- app/models/station.rb
+++ app/models/station.rb
@@ -5,6 +5,16 @@ class Station
     @fuel_type_code = attributes[:fuel_type_code]
     @access_days_time = attributes[:access_days_time]
   end
-end
 
-    # @Name, Address, Fuel Types, Distance, and Access Times
+  def self.all_by_state(state)
+    propublica.members_by_state(state).map do |raw_member|
+      Member.new(raw_member)
+    end
+  end
+
+  private
+
+  def self.propublica
+    PropublicaService.new
+  end
+end
--- app/services/nrel_service.rb
+++ app/services/nrel_service.rb
@@ -1,8 +1,20 @@
 class NrelService
   def initialize
-    @connection = Faraday.new(url: "https://api.propublica.org") do |faraday|
-      faraday.headers['X-API-Key'] = ENV['PROPUBLICA_API_KEY']
+    @connection = Faraday.new(url: "https://developer.nrel.gov") do |faraday|
       faraday.adapter Faraday.default_adapter
     end
+  end
+
+  def stations_by_fuel_type(fuel_type)
+    get_json("/api/alt-fuel-stations/v1.json?fuel_type=#{fuel_type}&limit=10&api_key=yEtMH5JrzYgDAYJv1iobx3sqqgaEGWvYSrOUQg1O&format=JSON")[:results]
+  end
+
+  private
+
+  attr_reader :connection
+
+  def get_json(url)
+    response = connection.get(url)
+    JSON.parse(response.body, symbolize_names: true)
   end
 end
