75c308510f0d7c94fd99cfcde8e99b485f8f3294
randy-darsh <rspringer47@gmail.com>
2017-12-15 11:53:20 -0700

    refactored to work with filter

--- app/services/nrel_service.rb
+++ app/services/nrel_service.rb
@@ -1,24 +1,40 @@
 class NrelService
-  def initialize
-    @connection = Faraday.new(url: "https://api.data.gov") do |faraday|
-      faraday.headers['X-Api-Key'] = ENV["NREL_KEY"]
-      faraday.adapter Faraday.default_adapter
+
+  def initialize(filter = {})
+    @filter = filter
+  end
+
+  def stations
+    response = Faraday.get("https://api.data.gov/nrel/alt-fuel-stations/v1/nearest.json?api_key=#{ENV["NREL_KEY"]}&location=#{filter[:location]}&limit=10&radius=6.0&fuel_type=ELEC,LPG")
+    @stations = JSON.parse(response.body, symbolize_names: true)[:fuel_stations].map do |raw_station|
+      Station.new(raw_station)
     end
   end
-
-  def stations_by_zip(zip_code)
-    get_json("/nrel/alt-fuel-stations/v1/nearest.json?fuel_type=LPG,ELEC&location=#{zip_code}&radius=6&limit=10")
-    # get_json("/api/alt-fuel-stations/v1.json?zip=#{zip_code}&api_key=yEtMH5JrzYgDAYJv1iobx3sqqgaEGWvYSrOUQg1O&format=JSON")[:results]
-  end
-  # this works for finding the nearest 10 stations now
-  # https://api.data.gov/nrel/alt-fuel-stations/v1/nearest.json?fuel_type=LPG,ELEC&location=80203&radius=6&limit=10&api_key=yEtMH5JrzYgDAYJv1iobx3sqqgaEGWvYSrOUQg1O
+  
   private
 
-  attr_reader :connection
+  attr_reader :filter
 
-  def get_json(url)
-    response = connection.get(url)
-    JSON.parse(response.body, symbolize_names: true)[:fuel_stations]
-    # JSON.parse(response.body, symbolize_names: true)
-  end
+  # def initialize
+  #   @connection = Faraday.new(url: "https://api.data.gov") do |faraday|
+  #     faraday.headers['X-Api-Key'] = ENV["NREL_KEY"]
+  #     faraday.adapter Faraday.default_adapter
+  #   end
+  # end
+  
+  # def stations_by_zip(zip_code)
+  #   get_json("/nrel/alt-fuel-stations/v1/nearest.json?fuel_type=LPG,ELEC&location=#{zip_code}&radius=6&limit=10")
+  #   # get_json("/api/alt-fuel-stations/v1.json?zip=#{zip_code}&api_key=yEtMH5JrzYgDAYJv1iobx3sqqgaEGWvYSrOUQg1O&format=JSON")[:results]
+  # end
+  # this works for finding the nearest 10 stations now
+  # https://api.data.gov/nrel/alt-fuel-stations/v1/nearest.json?fuel_type=LPG,ELEC&location=80203&radius=6&limit=10&api_key=yEtMH5JrzYgDAYJv1iobx3sqqgaEGWvYSrOUQg1O
+  # private
+
+  # attr_reader :connection
+
+  # def get_json(url)
+  #   response = connection.get(url)
+  #   JSON.parse(response.body, symbolize_names: true)[:fuel_stations]
+  #   # JSON.parse(response.body, symbolize_names: true)
+  # end
 end
